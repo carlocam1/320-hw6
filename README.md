810a838365fccfc78e44ca58393f90509f22aafe
carlocam1 <114327906+carlocam1@users.noreply.github.com>
2022-11-07 20:33:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 320-hw6
+ Home page displays the objects in the JSON variable, and when clicking on one object it shows the details of the item
---
+++ app/app.js
@@ -0,0 +1,186 @@
+// import { changePageContent } from "../model/model.js";
+
+var CAMP = [
+    {
+        campTitle: "Camping Esentials",
+        campThumbing: "camp-essentials_150x150.jpg",
+        campFullImg: "camp-essentials_500x500.jpg",
+        itemReview: "reviews.png",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+        campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+        campRating: "A",
+        itemPrice: "$44.95",
+    },
+    
+    {
+        campTitle: "Camping Chair",
+        campThumbing: "camp-chair_150x150.jpg",
+        campFullImg: "camp-chair_500x500.jpg",
+        itemReview: "reviews.png",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+        campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+        campRating: "B",
+        itemPrice: "$73.99",
+    },
+
+    {
+      campTitle: "Camping Backpack",
+      campThumbing: "camp-backpack_150x150.jpg",
+      campFullImg: "camp-backpack_500x500.jpg",
+      itemReview: "reviews.png",
+      campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+      campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+      campRating: "C",
+      itemPrice: "$49.95",
+  },
+
+  {
+    campTitle: "Camping Fridge",
+    campThumbing: "camp-fridge_150x150.jpg",
+    campFullImg: "camp-fridge_500x500.jpg",
+    itemReview: "reviews.png",
+    campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+    campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+    campRating: "D",
+    itemPrice: "$230.79",
+  },
+
+  {
+    campTitle: "Camping Lantern",
+    campThumbing: "camp-lantern_150x150.jpg",
+    campFullImg: "camp-lantern_500x500.jpg",
+    itemReview: "reviews.png",
+    campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+    campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+    campRating: "E",
+    itemPrice: "$16.17",
+  },
+
+  {
+    campTitle: "Camping Stove",
+    campThumbing: "camp-stove_150x150.jpg",
+    campFullImg: "camp-stove_500x500.jpg",
+    itemReview: "reviews.png",
+    campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+    campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+    itemPrice: "$69.00",
+  },
+
+  {
+    campTitle: "Camping Table",
+    campThumbing: "camp-table_150x150.jpg",
+    campFullImg: "camp-table_500x500.jpg",
+    itemReview: "reviews.png",
+    campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+    campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+    itemPrice: "$93.44",
+  },
+
+  {
+    campTitle: "Instant Tent",
+    campThumbing: "instant-tent_150x150.jpg",
+    campFullImg: "instant-tent_500x500.jpg",
+    itemReview: "reviews.png",
+    campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+    campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+    itemPrice: "$60.20",
+  },
+
+  {
+    campTitle: "Kid's Sleeping bag",
+    campThumbing: "kid-sleep-bag_150x150.jpg",
+    campFullImg: "kid-sleep-bag_500x500.jpg",
+    itemReview: "reviews.png",
+    campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+    campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+    itemPrice: "$22.45",
+  },
+
+
+  {
+    campTitle: "LED Lantern",
+    campThumbing: "led-lantern_150x150.jpg",
+    campFullImg: "led-lantern_500x500.jpg",
+    itemReview: "reviews.png",
+    campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt?",
+    campDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia consequatur quibusdam, laboriosam, maiores hic eos quod harum quam voluptatem atque molestias repudiandae totam nostrum tempore aliquid? Quibusdam, voluptate quam. Voluptatem amet nobis natus, laborum reiciendis deserunt, debitis aut omnis atque voluptas aspernatur, animi placeat. Fugiat eos tempora aliquid nemo in?",
+    campRating: "J",
+    itemPrice: "$14.95",
+  },
+];
+
+function loadData (){
+    $.each(CAMP, function(idx, camp){
+        $("#app").append(`<div id="${idx}" class="camp-holder">
+        <h5>${camp.campTitle}</h5>
+        <div class="camp-image">
+          <img src="images/camps_thumb/${camp.campThumbing}" alt="${camp.campTitle}" />
+        </div>
+        <div class="brief-rating">
+          <div class="brief-des">
+            ${camp.campBriefDescription}
+          </div>
+          <div class="thumb-price">Price: ${camp.itemPrice}</div>
+        </div>
+      </div>`);
+    });
+
+    initListeners(); 
+}
+
+// import { changePageContent } from "../model/model.js";
+
+function initListeners() {
+    $(".bars").click(function(e){
+        // console.log("clicked");
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+    });
+
+    $(".links a").click(function(e){
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+        // $(".links").removeClass("active");
+    });
+
+    $(".camp-holder").click(function(e){
+      let campIndex = e.currentTarget.id;
+      console.log(campIndex);
+      
+      $("#app").html(`<div class="camp">
+      <div class="camp-container">
+        <div class="campImg">
+          <img src="images/camp_full/${CAMP[campIndex].campFullImg}" alt="" />
+        </div>
+          <div class="campDes">
+            <h2>${CAMP[campIndex].campTitle}</h2>
+            ${CAMP[campIndex].campDescription}
+            <div class="reviews">
+              <img src="images/${CAMP[campIndex].itemReview}" alt="" />
+            </div>  
+            <div class="price">
+              <h1>${CAMP[campIndex].itemPrice}</h1>
+            </div>
+            <div class="button">add to car</div>
+
+        </div>   
+      </div>  
+      <div class="close button">CLOSE</div>  
+    </div>`);
+
+      addCloseListener();
+
+    });
+}
+
+function addCloseListener(){
+  $(".close").click(function(){
+    $("#app").html("");
+    loadData();
+  });
+}
+
+
+$(document).ready(function () {
+    loadData();    
+});
